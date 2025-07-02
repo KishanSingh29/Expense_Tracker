452bfcd320ea19b895afa7c014e33dc36f0ce632
KishanSingh29 <learnwithkishu2@gmail.com>
2025-07-02 22:37:05 +0530

    Adding UserDetails Security

---
+++ ExpenseTracker/src/main/java/com/BackendX/ExpenseTracker/service/CustomUserDetails.java
@@ -0,0 +1,49 @@
+package com.BackendX.ExpenseTracker.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+    private String username;
+
+    private String password;
+
+    Collection<? extends GrantedAuthority> authorities;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+
+   
+    public boolean isCredentialsNonExoired(){
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled(){
+        return true;
+    }
+}
